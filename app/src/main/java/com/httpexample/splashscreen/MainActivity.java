764c6dec91e054b8269b033d5f1b33eb94639e7b
kizzykode <kishoremaddy.g@gmail.com>
2018-06-13 10:22:00 +0530

    Initial commit

---
+++ app/src/main/java/com/httpexample/splashscreen/MainActivity.java
@@ -0,0 +1,27 @@
+package com.httpexample.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    int splash_time_out = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splash_time_out);
+    }
+}
